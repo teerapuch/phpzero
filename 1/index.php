ee055eb579f45a724b2309d9acb75404e569b4f2
teerapuch <teerapuch@hotmail.com>
2019-02-06 09:24:11 +0700

    update code in youtube series phpzero start coding

---
+++ 1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1><?php echo 'Hello PHP Zero'; ?></h1>
+        <?php
+            echo 'Hello Teerapuch';
+        ?>
+        <br>
+        <?php echo 'Hello Zero'; ?>
+    </body>
+</html>
