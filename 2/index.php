2183b41ee24cccdbac49c2f608975bd94877f99b
teerapuch <teerapuch@hotmail.com>
2019-02-06 10:27:54 +0700

    update variables

---
+++ 2/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Variables</title>
+    </head>
+    <body>
+        <?php
+            $product = 'box';
+            $productCategory = 'Packaging';
+            $product_group = 'Group';
+            $productCategoryType = "Type";
+            $box1 = '';
+            $box2 = "";
+            $box3 = null;
+            $box4 = 0;
+            echo $box4;
+            //define("AGE", 5);
+            //echo AGE;
+            // $a = 1;
+            # $b = '2';
+            // $c = "3";
+            /*
+            a b
+            c d
+            */
+            //echo $a."<br>";
+            //echo $name;
+            //$name = "PHPZero";
+            /*
+            echo 'Hello $name';
+            echo "<br>";
+            echo "Hello $name";
+            */
+        ?>
+    </body>
+</html>
