b7f09a53c0d8c656110686bcf872ed03bf57b729
teerapuch <teerapuch@hotmail.com>
2019-02-19 12:02:00 +0700

    PHPZero EP.5 Numbers

---
+++ 3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Numbers</title>
+    </head>
+    <body>
+        <h1>Numbers</h1>
+        <?php
+        // + - * / ++ --
+        $a = 20;
+        $b = 10;
+        $c = 5;
+        $d = 12;
+        $e = $a + ($d - $c) - $b;
+        $e--;
+        echo $e;
+        ?>
+    </body>
+</html>
